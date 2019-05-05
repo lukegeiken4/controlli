4f744c6145036ba4d979e4377718b63b98f297b0
Luke Geiken <lugeiken@microsoft.com>
2019-05-05 16:03:01 -0700

    update endpoint param model for easier use inline

--- src/models/params.ts
+++ src/models/params.ts
@@ -4,4 +4,21 @@ export class EndpointParams {
     public requiredNumbers: string[] = [];
     public optionalBooleans: string[] = [];
     public optionalNumbers: string[] = [];
+
+    public constructor(
+        fields?: {
+            requiredStrings?: string[],
+            requiredBooleans?: string[],
+            requiredNumbers?: string[],
+            optionalBooleans?: string[],
+            optionalNumbers?: string[],
+    }) {
+        if (fields) {
+            this.requiredStrings = fields.requiredStrings || this.requiredStrings;       
+            this.requiredBooleans = fields.requiredBooleans || this.requiredBooleans;        
+            this.requiredNumbers = fields.requiredNumbers || this.requiredNumbers;
+            this.optionalBooleans = fields.optionalBooleans || this.optionalBooleans;        
+            this.optionalNumbers = fields.optionalNumbers || this.optionalNumbers;       
+        }
+    }
 }
